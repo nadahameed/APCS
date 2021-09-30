a922f3f903dde1f3a4a9068a58c220737b8fd19a
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-09-30 12:31:09 -0400

    Add files via upload

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,8 @@
+public class Greet {
+	public static void main(String[] args){
+		Greet(a);
+	}
+	public static void Greet(String a){
+	System.out.println("Are you French? Because Eiffel for you," + "Nada");
+}
+}
