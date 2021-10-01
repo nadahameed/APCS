736d295f73ad0aaa12a90edf20971901ae7a228d
Nada <nhameed30@stuy.edu>
2021-10-01 12:31:24 -0400

    init setup

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,11 @@
+public class Greet {
+    public static void main( String[] args ) {
+        BigSibgreet("Flim");
+        BigSibgreet("Flam");
+        BigSibgreet("Kazaam");
+    }
+    
+    public static void BigSibgreet(String x) {
+    	System.out.println("Hello there, " + x);
+    }
+}
