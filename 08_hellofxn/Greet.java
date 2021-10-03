5efb499667cb8c27897ae26be3a70ec94d5a81bb
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-03 17:47:42 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,3 +1,13 @@
+/*
+Nada Hameed
+APCS
+HW08 -- Refactor Freshie Zero
+10/01/2021
+
+DISCOVERIES: All three are printed (Nada/Ray/Mr. Mykolyk + greeting)
+UNRESOLVED QUESTIONS: Is there a way to choose only one to print (greet only one person) when running the code?
+*/
+
 public class Greet {
 	public static void main(String[] args){
 		Greet("Nada");
