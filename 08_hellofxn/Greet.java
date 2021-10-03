977187e9a9dc4aa3d843b0774065e7a0bfdacc31
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-03 17:13:54 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,8 +1,10 @@
 public class Greet {
 	public static void main(String[] args){
-		Greet(a);
+		Greet("Nada");
+		Greet("Ray");
+		Greet("Mr. Mykolyk");
 	}
 	public static void Greet(String a){
-	System.out.println("Are you French? Because Eiffel for you," + "Nada");
+	System.out.println("Are you French? Because Eiffel for you," + a);
 }
 }
