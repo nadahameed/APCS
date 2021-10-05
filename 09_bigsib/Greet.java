aea503e31b67980e6c1c5d3e8a48110e9baad6d6
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-05 19:04:43 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,7 +1,7 @@
 /*
 Roaring '20s - Nada Hameed + Ray, Jeffery Tang + Mathias, Jefford Shau + Dylan
 APCS
-HW09 -- What are BigSibs Good For?
+HW09 -- Greet calls class BigSib calls method greet
 10/04/2021
 */
 
@@ -13,20 +13,11 @@ public class Greet {
     }
 }
 
-/*public class BigSib {
-    public static void greet(String name){
-        System.out.println("Hey there, " + name + ". How are you today?");
-    }
-}
-        
- public class Greet {
-	public static void main(String[] args){
-		Greet("Nada");
-		Greet("Ray");
-		Greet("Mr. Mykolyk");
-	}
-	public static void Greet(String a){
-	System.out.println("Hey there, " + a + ". How are you today?");
-}
-}
+/*
+DISCOVERIES:
+- BigSib.java doesn't need a main method because you never directly run BigSib.java.
+ -> Instead, you run BigSib.java through Greet.java, which does need a main method.
+
+UNRESOLVED QS:
+-
 */
