4304eeaeb48e4f811e88dc0931c7c94d53514d7e
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-05 18:51:19 -0400

    Create BigSib.java

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,13 @@
+public class BigSib {
+    public static void main( String[] args ) {
+        
+      
+      BigSibgreet("Flim");
+        BigSibgreet("Flam");
+        BigSibgreet("Kazaam");
+    }
+    
+    public static void greet(String x) {
+    	System.out.println("Hello there, " + x);
+    }
+}
