a1567121b5d417f414a21e41117889abdf01dffd
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-05 19:12:16 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -4,26 +4,17 @@ APCS
 HW10 -- Refactor Big Sib One
 10/05/2021
 */
+
+public class Greet {
+    public static void main( String[] args ) {
+        BigSib.greet("Crosby");
+        BigSib.greet("Stills");
+        BigSib.greet("Nash");
+    }
+}
+
 public class Greet {
     public static void main( String[] args ){
         BigSib.greet("Why hello, " + name);
     }
 }
-
-public class BigSib {
-    public static void main( String[] args ){
-        
-    public static String greet(String name) {
-        name = 
-    /*
-public class Greet {
-    public static void main( String[] args ){
-        System.out.println(BigSibgreet("Crosby") );
-        System.out.println(BigSibgreet("Stills") );
-        System.out.println(BigSibgreet("Nash") );
-    }
-    public static String BigSib.greet(String x) {
-        return ("Why hello, " + x);
-    }
-}
-*/
