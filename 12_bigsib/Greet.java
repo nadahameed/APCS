ce457e05c6a8d1447c9ed136a319eb5909f34c54
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-05 19:42:08 -0400

    Create Greet.java

---
+++ 12_bigsib/Greet.java
@@ -0,0 +1,16 @@
+/*
+
+*/
+
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+
+    BigSib richard = new BigSib();
+    richard.setHelloMsg("Word up");
+
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+
+  } //end main()
+}
