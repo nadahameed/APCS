abe65917adf59d9b8eb7f6447ee02b2a1e53511b
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-05 19:10:37 -0400

    Update BigSib.java

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -1,13 +1,12 @@
+/*
+Team Not-in-Service - Nada Hameed + Ray, Fang Min Chen + Cat, Prattay Dey + Winnie
+APCS
+HW10 -- Greet calls class BigSib calls method greet to produce personalized greetings --> this is class BigSib
+10/05/2021
+*/
+
 public class BigSib {
-    public static void main( String[] args ) {
-        
-      
-      BigSibgreet("Flim");
-        BigSibgreet("Flam");
-        BigSibgreet("Kazaam");
-    }
-    
-    public static void greet(String x) {
-    	System.out.println("Hello there, " + x);
+    public static void greet(String name){
+        System.out.println("It's quite nice to see you, " + name + "!");
     }
 }
