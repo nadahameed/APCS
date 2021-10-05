ce635ebf3a83e1a158e744c1a05404984f1478ee
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-04 22:08:46 -0400

    Create Greet.java

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,14 @@
+/*
+Team Not-In-Service: Nada Hameed + Ray, Fang Min Chen + Cat, Prattay Dey + Winnie
+APCS
+HW10 -- Refactor Big Sib One
+10/05/2021
+*/
+
+public class Greet {
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
+}
