ea559ad3239a6e09809c0b1ba5bd1a4a56a44dbb
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-05 20:43:05 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,6 +1,10 @@
 /*
-
+Nada Hameed, Hugo Jenkins
+APCS
+HW12 -- returns greetings specific to each big sib -- this is class BigSib called by Greet.java
+10/06/2021
 */
+
 public class Greet {
   public static void main( String[] args ) {
     String greeting;
@@ -31,3 +35,11 @@ public class Greet {
 
   }
 }
+
+/*
+DISCOVERIES:
+- the names (richard, remy, raine, and rorie) don't show up at all in the code - they are just representing each "big sib" with personalized greetings not direct to them.
+
+UNRESOLVED QS:
+-
+*/
