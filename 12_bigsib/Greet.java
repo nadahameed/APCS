21fdc9784deb978f0dcb9514d0504fb25d7ea48e
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-05 20:08:28 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,7 +1,6 @@
 /*
 
 */
-
 public class Greet {
   public static void main( String[] args ) {
     String greeting;
@@ -11,6 +10,24 @@ public class Greet {
 
     greeting = richard.greet("freshman");
     System.out.println(greeting);
+    
+    BigSib remy = new BigSib();
+    richard.setHelloMsg("Salutations");
 
-  } //end main()
+    greeting = remy.greet("Dr. Spaceman");
+    System.out.println(greeting);
+    
+    BigSib raine = new BigSib();
+    raine.setHelloMsg("Hey ya");
+
+    greeting = raine.greet("Kong Fooey");
+    System.out.println(greeting);
+    
+    BigSib rorie = new BigSib();
+    rorie.setHelloMsg("Sup");
+
+    greeting = rorie.greet("mom");
+    System.out.println(greeting);
+
+  }
 }
