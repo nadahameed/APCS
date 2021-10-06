d94933131eba193cf4c01a692ec0e1ff548583ec
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-05 20:08:03 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -7,11 +7,55 @@
 
     greeting = richard.greet("freshman");
     System.out.println(greeting);
+    
+    BigSib remy = new BigSib();
+    richard.setHelloMsg("Salutations");
 
-  } //end main() */
+    greeting = remy.greet("Dr. Spaceman");
+    System.out.println(greeting);
+    
+    BigSib raine = new BigSib();
+    raine.setHelloMsg("Hey ya");
+
+    greeting = raine.greet("Kong Fooey");
+    System.out.println(greeting);
+    
+    BigSib rorie = new BigSib();
+    rorie.setHelloMsg("Sup");
+
+    greeting = rorie.greet("mom");
+    System.out.println(greeting);
+
+  }
+  }
+  
+  result:
+  $ javac Greet.java
+Word up freshman
+Salutations Dr. Spaceman
+Hey ya Kong Fooey
+Sup mom
+  
+  
+  */
+
+
 
 public class BigSib {
-  public static void {
-    
+  public static void setHelloMsg(String helloMsg){
+    String a = helloMsg;
   }
-  public static void 
+  public static void greet(String name){
+    String x = name;
+  }
+  greeting = a + " " + x;
+}
+
+  
+ /*   public class BigSib {
+    public static void greet(String name){
+        System.out.println("It's quite nice to see you, " + name + "!");
+    }
+}
+
+*/
