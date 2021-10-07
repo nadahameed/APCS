71bb1dc6bbfea9e1f643e388e8dacc4098f32280
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-06 20:35:37 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -9,26 +9,22 @@ public class Greet {
   public static void main( String[] args ) {
     String greeting;
 
-    BigSib richard = new BigSib();
-    richard.setHelloMsg("Word up");
+    BigSib richard = new BigSib("Word up");
 
     greeting = richard.greet("freshman");
     System.out.println(greeting);
     
-    BigSib remy = new BigSib();
-    richard.setHelloMsg("Salutations");
+    BigSib remy = new BigSib("Salutations");
 
     greeting = remy.greet("Dr. Spaceman");
     System.out.println(greeting);
     
-    BigSib raine = new BigSib();
-    raine.setHelloMsg("Hey ya");
+    BigSib raine = new BigSib("Hey ya");
 
     greeting = raine.greet("Kong Fooey");
     System.out.println(greeting);
     
-    BigSib rorie = new BigSib();
-    rorie.setHelloMsg("Sup");
+    BigSib rorie = new BigSib("Sup");
 
     greeting = rorie.greet("mom");
     System.out.println(greeting);
