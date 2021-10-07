639dc35eded741ef1efeeb54dd17731ab8eeb5c1
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-06 20:07:24 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -1,1 +1,37 @@
+/*
+Nada Hameed, Hugo Jenkins
+APCS
+HW13 -- using a constructor
+10/07/2021
+*/
 
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+
+    BigSib richard = new BigSib();
+    richard.setHelloMsg("Word up");
+
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+    
+    BigSib remy = new BigSib();
+    richard.setHelloMsg("Salutations");
+
+    greeting = remy.greet("Dr. Spaceman");
+    System.out.println(greeting);
+    
+    BigSib raine = new BigSib();
+    raine.setHelloMsg("Hey ya");
+
+    greeting = raine.greet("Kong Fooey");
+    System.out.println(greeting);
+    
+    BigSib rorie = new BigSib();
+    rorie.setHelloMsg("Sup");
+
+    greeting = rorie.greet("mom");
+    System.out.println(greeting);
+
+  }
+}
