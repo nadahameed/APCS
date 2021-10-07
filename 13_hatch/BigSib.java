ab699f1052dd137deba13f32d2d0e25a65dbeaf0
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-06 20:40:45 -0400

    Update BigSib.java

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,9 +1,37 @@
+/*
+NHS -- Nada Hameed + Ray, Hugo Jenkins + Boary
+APCS
+HW13 -- using a constructor method
+10/07/2021
+*/
+
 public class BigSib {
   String helloMsg;
   public BigSib(String a){
-    a = helloMsg;
+    helloMsg = a;
   }
   public String greet(String name){
     return(helloMsg + " " + name);
   }
 }
+
+/*
+DISCOVERIES:
+- This is basically a shortcut of 12_bigsib
+- Earlier code:
+  
+  public class BigSib {
+    String helloMsg;
+    public BigSib(String a){
+      a = helloMsg;
+    }
+    public String greet(String name){
+      return(helloMsg + " " + name);
+    }
+  }
+  
+  -> returned null [name] for all four big sibs
+  
+UNRESOLVED QS:
+- 
+*/
