943447ec1caeec56efff6257b2aa6ae9c2cdc650
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-07 21:12:31 -0400

    Update BigSib.java

--- 14_overload/overcon/BigSib.java
+++ 14_overload/overcon/BigSib.java
@@ -1,21 +1,8 @@
 /*
-public class Greet{
-  public static void main(String[] args){
-    String greeting;
-    BigSib richard = new BigSib();
-    BigSib grizz = new BigSib();
-    BigSib dotCom = new BigSib();
-    BigSib tracy = new BigSib();
-    greeting = richard.greet("freshman");
-    System.out.println(greeting);
-    greeting = tracy.greet("Dr. Spaceman");
-    System.out.println(greeting);
-    greeting = grizz.greet("Kong Fooey");
-    System.out.println(greeting);
-    greeting = dotCom.greet("mom");
-    System.out.println(greeting);
-  }
-}
+HPN - Nada Hameed + Ray, Hugo Jenkins + Boary, Prattay Dey + Winnie
+APCS
+HW14 - using contructors
+10/08/2021
 */
 
 public class BigSib {
@@ -27,3 +14,14 @@ public class BigSib {
     return(helloMsg + " " + name);
   }
 }
+
+/*
+DISCOVERIES:
+- single quotations can only take a single character in java.
+- a contructor with no parameters still runs and can be useful in many cases.
+- the difference between the Greet files is that one of them takes an argument while creating the new instance of BigSib
+
+QCC:
+- can a constructor take multiple arguments?
+- what other parameters could we include to make the code more efficient?
+*/
