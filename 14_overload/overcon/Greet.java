53551521c895f99c67e3289c8cdd5fd68bc52537
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-07 21:11:45 -0400

    Update Greet.java

--- 14_overload/overcon/Greet.java
+++ 14_overload/overcon/Greet.java
@@ -1,3 +1,10 @@
+/*
+HPN - Nada Hameed + Ray, Hugo Jenkins + Boary, Prattay Dey + Winnie
+APCS
+HW14 - using contructors
+10/08/2021
+*/
+
 public class Greet {
   public static void main(String[] args){
     String greeting;
@@ -15,3 +22,14 @@ public class Greet {
     System.out.println(greeting);
   }
 }
+
+/*
+DISCOVERIES:
+- single quotations can only take a single character in java.
+- a contructor with no parameters still runs and can be useful in many cases.
+- the difference between the Greet files is that one of them takes an argument while creating the new instance of BigSib
+
+QCC:
+- can a constructor take multiple arguments?
+- what other parameters could we include to make the code more efficient?
+*/
