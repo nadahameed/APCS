ce0789005d3f325d113c25be459bf7c9d145d099
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-07 20:46:50 -0400

    Update BigSib.java

--- 14_overload/defcon/BigSib.java
+++ 14_overload/defcon/BigSib.java
@@ -19,3 +19,13 @@ public class Greet {
 }
 
 */
+
+public class BigSib {
+  String helloMsg;
+  public BigSib(String a){
+    helloMsg = a;
+  }
+  public String greet(String name){
+    return(helloMsg + " " + name);
+  }
+}
