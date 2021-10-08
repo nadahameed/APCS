68cdea6a099fe73d1ad2d18be46133222992d539
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-07 20:47:55 -0400

    Update Greet.java

--- 14_overload/defcon/Greet.java
+++ 14_overload/defcon/Greet.java
@@ -1,5 +1,5 @@
 public class Greet {
-  public static vois main(String[] args){
+  public static void main(String[] args){
     String greeting;
     BigSib richard = new BigSib("Word Up");
     BigSib grizz = new BigSib("Hey ya");
