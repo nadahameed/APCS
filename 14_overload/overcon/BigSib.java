cfaf80ace5d4ec65659293f058faeb76d64e0c61
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-10-07 20:58:50 -0400

    Update BigSib.java

--- 14_overload/overcon/BigSib.java
+++ 14_overload/overcon/BigSib.java
@@ -1,3 +1,23 @@
+/*
+public class Greet{
+  public static void main(String[] args){
+    String greeting;
+    BigSib richard = new BigSib();
+    BigSib grizz = new BigSib();
+    BigSib dotCom = new BigSib();
+    BigSib tracy = new BigSib();
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+    greeting = tracy.greet("Dr. Spaceman");
+    System.out.println(greeting);
+    greeting = grizz.greet("Kong Fooey");
+    System.out.println(greeting);
+    greeting = dotCom.greet("mom");
+    System.out.println(greeting);
+  }
+}
+*/
+
 public class BigSib {
   String helloMsg;
   public BigSib(String a){
@@ -6,3 +26,4 @@ public class BigSib {
   public String greet(String name){
     return(helloMsg + " " + name);
   }
+}
