f409a31bd92ef69639808236a9e2897e52cf7359
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-11-01 22:22:23 -0400

    Update DoubleChar.java

--- cb/string-2/DoubleChar.java
+++ cb/string-2/DoubleChar.java
@@ -6,10 +6,20 @@ doubleChar
 
 */
 
-public String doubleChar(String str) {
-  String a = "";
-  for (int i = 0; i < str.length(); i++) {
-    a += str.substring (i);
+public class DoubleChar {
+
+  public static String doubleChar(String str) {
+    String a = "";
+    for (int i = 0; i < str.length(); i++) {
+      String b = str.substring (i, i + 1);
+      System.out.println(i + " -> " + b);
+      a += b + b;
+      System.out.println(" a -> " + a);
+     }
+     return (a);
   }
-  return a;
+
+  public static void main(String[] args) {
+    System.out.println(doubleChar("cat"));
+  }
 }
