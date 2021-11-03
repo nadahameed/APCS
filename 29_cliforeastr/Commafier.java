73345dd6c8e0438df517e6627ce1990279fba835
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-11-02 21:48:28 -0400

    Create Commafier.java

---
+++ 29_cliforeastr/Commafier.java
@@ -0,0 +1,18 @@
+/*
+
+APCS
+HW 29 - Which Way Do You Roll?
+
+*/
+
+public class Commafier{
+  
+  public static String commafyR (int i){
+    String integer = "";
+    integer = i.toString();
+  }
+  
+  public static void main(String[] args){
+    System.out.println(commafyR(8));
+  }
+}
