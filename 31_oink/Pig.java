1b2ff31071ff2b43f6c55927ad063fd38979a836
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-11-03 19:20:54 -0400

    Update Pig.java

---
+++ 31_oink/Pig.java
@@ -0,0 +1,84 @@
+/*
+
+Team NBJ - Nada Hameed, Brian Kang, Jun Hong Wang
+APCS
+HW31 - Otnay Ybay ethay Airhay Onway Ymay Inneechay Inchay Inchay - Pig Latin
+2021-10-04
+time spent:
+
+DISCO:
+
+
+QCC:
+
+
+*/
+
+public class Pig
+{
+  //Q: How does this initialization make your life easier?
+  private static final String VOWELS = "aeiou";
+
+
+  /*=====================================
+    boolean hasA(String,String) -- checks for a letter in a String
+    pre:  w != null, letter.length() == 1
+    post: hasA("cat", "a") -> true
+    hasA("cat", "p")       -> false
+    =====================================*/
+  public static boolean hasA( String w, String letter ) 
+  {
+    /* YOUR IMPLEMENTATION HERE */
+  }//end hasA()
+
+
+  /*=====================================
+    boolean isAVowel(String) -- tells whether a letter is a vowel
+    precondition: letter.length() == 1
+    =====================================*/
+  public static boolean isAVowel( String letter ) 
+  {
+    /* YOUR IMPLEMENTATION HERE */
+  }
+
+
+  /*=====================================
+    int countVowels(String) -- counts vowels in a String
+    pre:  w != null
+    post: countVowels("meatball") -> 3
+    =====================================*/
+  public static int countVowels( String w ) 
+  {
+    /* YOUR IMPLEMENTATION HERE */
+  }
+
+
+  /*=====================================
+    boolean hasAVowel(String) -- tells whether a String has a vowel
+    pre:  w != null
+    post: hasAVowel("cat") -> true
+    hasAVowel("zzz")       -> false
+    =====================================*/
+  public static boolean hasAVowel( String w ) 
+  {
+    /* YOUR IMPLEMENTATION HERE */
+  }
+
+
+  /*=====================================
+    String allVowels(String) -- returns vowels in a String
+    pre:  w != null
+    post: allVowels("meatball") -> "eaa"
+    =====================================*/
+  public static String allVowels( String w ) 
+  {
+    /* YOUR IMPLEMENTATION HERE */
+  }
+
+
+  public static void main( String[] args ) 
+  {
+    /* YOUR TEST CALLS HERE */
+  }//end main()
+
+}//end class Pig
