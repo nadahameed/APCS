6f1aa070a9dfad328861fc476986262b994b5275
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-12-01 17:02:26 -0500

    Create Rational.java

---
+++ 41_rat/Rational.java
@@ -0,0 +1,50 @@
+/*
+
+TNPG: doubleN - Nicole Zhou + Duck, Nada Hameed + Ray
+APCS
+HW41: Be Rational
+2021-12-02
+time spent:
+
+DISCOS:
+- 
+
+QCCs:
+- 
+
+*/
+
+public class Rational{
+  
+  public int num;
+  public int den;
+  
+  public Rational(){
+    num = 0;
+    den = 1;
+  }
+  
+  public Rational(int p, int q){
+    num = p;
+    den = q;
+  }
+  
+  public String toString(){
+    String s = num + " / " + den;
+    return s;
+  }
+  
+  public floatValue(){
+    return (float)(num / den);
+  }
+  
+  public Multiply(Rational factor){
+    this.p = this.p * factor.p;
+    this.q = this.q * factor.q;
+  }
+  
+  public Divide(Rational factor){
+    this.p = this.p / factor.p;
+    this.q = this.q / factor.q;
+  }
+}
