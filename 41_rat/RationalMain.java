0bded4b166e08dcda7b7dfc449ffdac4a0822833
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-12-01 17:21:37 -0500

    Update RationalMain.java

--- 41_rat/RationalMain.java
+++ 41_rat/RationalMain.java
@@ -2,12 +2,12 @@ public class RationalMain{
   
   public static void main(String[] args){
     
-    Rational r = new Rational(2, 3);       //Stores rational number 2/3
-    Rational s = new Rational(1, 2);       //Stores rational number 1/2
+    Rational r = new Rational(2, 3);
+    Rational s = new Rational(1, 2);
     System.out.println(r.toString());
     System.out.println(s.toString());
     
     r.multiply(s);
-    System.out.println(r.toString());      //Multiplies r by s, changes r to 2/6.  s remains 1/2
+    System.out.println(r.toString());
   }
 }
