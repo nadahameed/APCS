c09923ff1f35de758805533ba7978f5e072e817f
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-11-30 20:09:56 -0500

    Update TwoDimArray.java

--- 40_arrxarr/TwoDimArray.java
+++ 40_arrxarr/TwoDimArray.java
@@ -72,4 +72,30 @@ public class TwoDimArray{
     }
     return summer;
   }
+  
+  public static void main( String [] args )
+  {
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      int [][] m1 = new int[4][2];
+      int [][] m2 = { {2,4,6}, {3,5,7} };
+      int [][] m3 = { {2}, {4,6}, {1,3,5} };
+      print1(m1);
+      print1(m2);
+      print1(m3);
+      print2(m1);
+      print2(m2);
+      print2(m3);
+      System.out.print("testing sum1...\n");
+      System.out.println("sum m1 : " + sum1(m1));
+      System.out.println("sum m2 : " + sum1(m2));
+      System.out.println("sum m3 : " + sum1(m3));
+      System.out.print("testing sum2...\n");
+      System.out.println("sum m1 : " + sum2(m1));
+      System.out.println("sum m2 : " + sum2(m2));
+      System.out.println("sum m3 : " + sum2(m3));
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+    // your own custom test cases welcomed and encouraged
+  }
+
 }//end class TwoDimArray
