2fad749a23a157e716fcaec1f530221235680359
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-12-06 19:11:33 -0500

    Update SuperArray.java

--- 43_suparr/SuperArray.java
+++ 43_suparr/SuperArray.java
@@ -3,12 +3,15 @@ TNPG: doubleN - Nicole Zhou + Duck, Nada Hameed + Ray
 APCS pd6
 HW43 -- encapsulation (basic SuperArray functionality)
 2021-12-07
+time spent: 30 mins
 
 DISCOS:
-- 
+- toString doesn't have to be called => it's now the default toString of _data
+  -> convenient
 
 QCCS:
-- 
+- how would we remove a term?
+  -> what's the alternative to left-justification after doing so?
 
 /***************************
  * class SuperArray
@@ -27,6 +30,7 @@ public class SuperArray
   public SuperArray()
   {
     /* YOUR IMPLEMENTATION HERE */
+    _data = new int[10];
   }
 
 
@@ -34,6 +38,12 @@ public class SuperArray
   public String toString()
   {
     /* YOUR IMPLEMENTATION HERE */
+    String s = "[";
+    for (int i : _data){
+      s = s + i + ",";
+    }
+    s = s.substring(0,s.length() - 1) + "]";
+    return s;
   }
 
 
@@ -41,6 +51,11 @@ public class SuperArray
   private void expand()
   {
     /* YOUR IMPLEMENTATION HERE */
+    int[] _newArray = new int[2 * _data.length];
+    for (int i = 0; i < _data.length; i++) {
+      _newArray[i] = _data[i];
+    }
+    _data = _newArray;
   }
 
 
@@ -48,6 +63,7 @@ public class SuperArray
   public int get( int index )
   {
     /* YOUR IMPLEMENTATION HERE */
+    return _data[index];
   }
 
 
@@ -56,16 +72,20 @@ public class SuperArray
   public int set( int index, int newVal )
   {
     /* YOUR IMPLEMENTATION HERE */
+    int oldVal = _data[index];
+    _data[index] = newVal;
+    return oldVal;
   }
 
 
   //main method for testing
   public static void main( String[] args )
   {
-    /*~~~~~~~~move~me~down~~~~~~~~~~~~~~V~~~~~~~~
+
       SuperArray curtis = new SuperArray();
       System.out.println( "Printing empty SuperArray curtis..." );
       System.out.println( curtis );
+
 
       for( int i = 0; i < curtis._data.length; i++ ) {
       curtis.set( i, i * 2 );
@@ -79,7 +99,6 @@ public class SuperArray
       System.out.println("Printing expanded SuperArray curtis...");
       System.out.println(curtis);
       }
-      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|~~~~~~~~*/
   }//end main()
 
 
