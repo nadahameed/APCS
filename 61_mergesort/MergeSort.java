28c01b2f5b1650af27f57ed9c2ee97da6faaac36
nadahameed <nhameed30@stuy.edu>
2022-02-08 20:08:33 -0500

    powwow rabbit

--- 61_mergesort/MergeSort.java
+++ 61_mergesort/MergeSort.java
@@ -3,7 +3,19 @@
   Implements mergesort on array of ints.
 
   Summary of Algorithm:
+    merge method:
+    - define two counters and set them to 0, one per inputed array
+    - compare the values at the indeces of the counters (respectively)
+    - add the lesser value to the new merge array
+    - increase the counter by one for the array with that lower value you just added
+    - continue comparing until there are no more elements left in an array
+    - add the remaining elements of the remaining array
 
+    sort method:
+    - create two arrays, of roughly the same length
+    - set all the values of the arrays to the values of the inputed array
+    -> differentiate between left and right (first 10 to left, last 10 to right)
+    - merge the two arrays with the previous method
   ***/
 
 public class MergeSort
@@ -17,17 +29,25 @@ public class MergeSort
    ******************************************************/
   private static int[] merge( int[] a, int[] b )
   {
-    int[] bear = new int[a.length+b.length];
+    int[] bear = new int[a.length + b.length];
     int actr = 0;
     int bctr = 0;
-    for(int i = 0; i < bear.length-1; i++){
-      if(a[actr] > b[bctr]){
+    for(int i = 0; i < bear.length; i++){
+      if(actr == a.length){
+        bear[i] = b[bctr];
+        bctr++;
+      }
+      else if(bctr == b.length){
         bear[i] = a[actr];
         actr++;
       }
-      else{
+      else if(a[actr] > b[bctr]){
         bear[i] = b[bctr];
         bctr++;
+      }
+      else{
+        bear[i] = a[actr];
+        actr++;
       }
     }
 
@@ -40,10 +60,22 @@ public class MergeSort
    * Sorts input array using mergesort algorithm
    * Returns sorted version of input array (ascending)
    ******************************************************/
-  public static int[] sort( int[] arr )
+  public static int[] sort( int[] fox )
   {
-
-  }//end sort()
+    if (fox.length <= 1) {
+      return fox;
+    }
+    int mid = (fox.length / 2);
+    int[] left = new int[mid];
+    int[] right = new int[fox.length - mid];
+    for (int i = 0; i < mid; i++) {
+      left[i] = fox[i];
+    }
+    for (int i = 0; i < right.length; i++) {
+      right[i] = fox[mid + i];
+    }
+    return merge(sort(left), sort(right));
+  }
 
 
 
@@ -68,7 +100,7 @@ public class MergeSort
   //main method for testing
   public static void main( String [] args )
   {
-    /*~~~~~~~~~~~~~~ Ye Olde Tester Bar ~~~~~~~~~~~~~~
+
       int[] arr0 = {0};
       int[] arr1 = {1};
       int[] arr2 = {1,2};
@@ -90,10 +122,19 @@ public class MergeSort
       printArray( merge(arr4,arr6) );
 
       System.out.println("\nSorting arr4-7...");
+
+      System.out.println("\nbefore sorting:\n");
+      printArray(( arr4 ) );
+      printArray(( arr5 ) );
+      printArray(( arr6 ) );
+      printArray(( arr7 ) );
+
+      System.out.println("\nafter sorting:\n");
       printArray( sort( arr4 ) );
       printArray( sort( arr5 ) );
       printArray( sort( arr6 ) );
       printArray( sort( arr7 ) );
+      /*~~~~~~~~~~~~~~ Ye Olde Tester Bar ~~~~~~~~~~~~~~
       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   }//end main()
 
