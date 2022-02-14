7cfa59cf333503e60e86ec5a3630c9289a62dd54
Nada <nhameed30@stuy.edu>
2022-02-14 13:18:30 -0500

    cbbc

--- cb/recursion-1/BunnyEars.java
+++ cb/recursion-1/BunnyEars.java
@@ -1,15 +1,15 @@
 
 public class BunnyEars{
 	public int bunnyEars(int bunnies){
-		if (bunnies == 0){
-			return 0;
-		}
-		else if(bunnies == 1){
+		if (bunnies == 1){
 			return 2;
 		}
-		return bunnyEars(bunnies) + bunnyEars(bunnies - 1);
+		if(bunnies == 0){
+			return 0;
+		}
+		return 2 + bunnyEars(bunnies - 1);
 	}
-	
+
 	public static void main(String[] args){
 		BunnyEars b = new BunnyEars();
 		System.out.println(b.bunnyEars(0));
---
+++ cb/recursion-1/Fibonacci.java
@@ -0,0 +1,12 @@
+public class Fibonacci{
+  public int fibonacci (int n){
+
+  }
+
+  public static void main(String[] args){
+    Fibonacci x = new Fibonacci();
+    System.out.println(x.fibonacci(0));
+    System.out.println(x.fibonacci(1));
+    System.out.println(x.fibonacci(2));
+  }
+}
