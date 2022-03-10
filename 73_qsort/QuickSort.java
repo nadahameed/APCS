a497a958469874db535a23fbca6cd0d927b9b2fb
nadahameed <90862244+nadahameed@users.noreply.github.com>
2022-03-09 21:55:00 -0500

    hw

--- 73_qsort/QuickSort.java
+++ 73_qsort/QuickSort.java
@@ -1,8 +1,8 @@
-//Clyde Sinclair
-//APCS pd0
+// Hot Cake: Nada Hameed, Samantha Hua, and Ariel Fuchs
+//APCS pd7
 //HW72 -- QuickSort
 //2022-03-09w
-//time spent: _h
+//time spent: 1.5h + class time
 
 /***
  * class QuickSort
@@ -10,13 +10,27 @@
  *
  * 1. Summary of QuickSort algorithm:
  * QSort(arr):
- *
+ * The algorithm begins by running partition on the entire array with the pivot
+ index of 0. Partition is run on the resulting halves of the array simulateously.
+ IF the array being QuickSorted on is an array of 1, we know that it has
+ reached it's final position. If not, the array if divided into two using the
+ final PvtPos and recursed.
+
  * 2a. Worst pivot choice and associated run time:
- * - the pivot is at the extremes
+ * The worst pivot choice is the index of the extremes of the array (max and min).
+ This would be bad because it would force the algorithm to repeat itself for
+ every value of n. This is why the run time efficiency is O(n^2)
+
  * 2b. Best pivot choice and associated run time:
- *
+ * The best pivot choice is the index of the median value in the array. This is
+ because it would allow for an even sorting of both halves. The run time would
+ be O(nlogn) similar to mergesort. This is because we divide and conquer in a
+ similar fashion.
+
  * 3. Approach to handling duplicate values in array:
- *
+ * We aren't sure because our algorithm is fully finished but we are considering
+ deleting and then readding the duplicate values at the end. However, this would
+ affect big oh because the algo would need to loop through the array once more.
  **/
 
 public class QuickSort
@@ -60,19 +74,41 @@ public class QuickSort
   }
   //--------------^  HELPER METHODS  ^--------------
 
-
-
-
   /**
    * void qsort(int[])
    * @param d -- array of ints to be sorted in place
    */
   public static void qsort( int[] d )
   {
+    int start = 0;
+    int end = d.length-1;
+
+    qsortHelper(d, start, end);
 
   }
 
   //you may need a helper method...
+
+  public static void qsortHelper( int[] d , int start, int end)
+  {
+    if (start + 1 == end || start > end) {
+      // an array of one is always sorted
+      return;
+    }
+
+    int finalPvtPos = partition(d, start, end, 0);
+
+    //left side of partition
+    if (finalPvtPos != 0 ) {
+      qsortHelper(d, start, finalPvtPos-1);
+    }
+
+    //right side of partition
+    if (finalPvtPos != d.length) {
+      qsortHelper(d, finalPvtPos+1, end);
+    }
+
+  }
 
   public static int partition( int arr[], int loPos, int hiPos, int pvtPos)
   {
@@ -91,34 +127,9 @@ public class QuickSort
     return s;
   }//end partition
 
-  public static int smallestElement(int[] arrayCake, int y){
-    int start = 0;
-    int end = arrayCake.length-1;
-    int pivot = 0;
-
-    pivot = partition(arrayCake, start, end, pivot);
-    while (pivot != y){
-      if (pivot > y){
-        end = pivot - 1;
-        pivot = end - 1;
-      }
-      else{
-        start = pivot;
-        pivot = start + 1;
-      }
-      pivot = partition(arrayCake, start, end, pivot);
-    }
-
-    return arrayCake[y-1];
-  }
-
-
-
-
   //main method for testing
   public static void main( String[] args )
   {
-    /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
 
     //get-it-up-and-running, static test case:
     int [] arr1 = {7,1,5,12,3};
@@ -144,6 +155,8 @@ public class QuickSort
     qsort( arrN );
     System.out.println("arrN after sort: " );
     printArr(arrN);
+    /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
+
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 
@@ -175,7 +188,6 @@ public class QuickSort
     System.out.println("arrMatey after sort: " );
     printArr(arrMatey);
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
   }//end main
 
 }//end class QuickSort
